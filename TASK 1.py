67d1be25e9daf713cce7a8291b8f1767a1846937
GodS-Law <rudraksharitik365@gmail.com>
2025-06-17 21:50:58 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,13 @@
+#This is for taking input in float values. f_no is first number and s_no is the second number.
+f_no = float(input("Enter the first number: "))
+s_no = float(input("Enter the second number: "))
+
+#For calculating.
+add = f_no + s_no
+sub = f_no - s_no
+multi = f_no * s_no
+div = f_no / s_no
+
+#For showing the values.
+print(f"Addition: {add}\nSubtraction: {sub}\nMultiplication: {multi}\nDivision: {div}")
+
---
+++ TASK 2.py
@@ -0,0 +1,6 @@
+#For taking the input
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+#For printing the greetings
+print(f"Hello, {f_name} {l_name}! Welcome to the Python program")
